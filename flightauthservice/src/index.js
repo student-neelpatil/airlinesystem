9a77375adbedab0aec4677a5e42d590deaf3c7b2
Neel Patil <patilneel288@gmail.com>
2025-09-24 17:31:29 +0530

    commiting

--- flightauthservice/src/index.js
+++ flightauthservice/src/index.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const {PORT}=require("./config/serverconfig");
+const logger=require("./config/logger-config")
 
 
 function serverconnection(){
     const app=express();
 
     app.listen(PORT,()=>{
-        console.log(`server is connected at port:${PORT}`)
+        logger.info(`server is connected at port:${PORT}`)
     })
 }
 
