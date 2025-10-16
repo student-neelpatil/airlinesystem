549d70bfaebf372f3c13ec072c657ccc218797c0
student-neelpatil <patilneel288@gmail.com>
2025-10-16 23:54:48 +0530

    adding error class in flightandsearch service

---
+++ flightsandsearch/src/utils/errors/index.js
@@ -0,0 +1,10 @@
+import  ValidationError  from "./validationerror"
+import  ServiceError   from "./serviceerror"
+import  AppError  from "./generalerror"
+
+
+modules.exports={
+    validationError:ValidationError,
+    appError:AppError,
+    serviceError:ServiceError
+}
